61a99b472853bb04078763205cda4aae0e83eaeb
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-01-18 13:04:19 +0200

    Fix DB

--- FlowerShop/settings.py
+++ FlowerShop/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Flower'
+    'Flower',
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
